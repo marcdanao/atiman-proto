3a30ee844e03ed851ee1a57bf9e06e3bd024f5d1
marcdanao <116094186+marcdanao@users.noreply.github.com>
2024-04-11 16:17:19 +0800

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
@@ -8,8 +7,7 @@ function App() {
   return (
     <>
      <Routes>
-      <Route path="/" element={<Home  />}/>
-      <Route path="/login" element={<Login  />}/>
+      <Route path="/" element={<Login  />}/>
       <Route path="/register" element={<Register  />}/>
      </Routes>
     </>
